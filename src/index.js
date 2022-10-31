08231c600acfbbdc50d9786823bbca473e80433f
Lore <ylfierro@sena.edu.co>
2022-10-31 09:06:27 -0500

    Mi primer commmit para el proyecto bajo el mar app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles.css';
+import { animales } from './animales';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const titulo = "";
+const mostrarFondo= true;
+const imagenes = [];
+
+
+
+for (const animal in animales) {
+  imagenes.push(
+    <img 
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animales[animal].imagen}
+      ariaLabel={animal}
+      role="button"
+      onClick={mostrarHecho}
+    />
+  )
+}
+
+function mostrarHecho(e) {
+  const animalSeleccionado = e.target.alt;
+  const animalInfo = animales[animalSeleccionado];
+  const indiceOpcion = Math.floor(Math.random() * animalInfo.hechos.length);
+  const hecho = animalInfo.hechos[indiceOpcion];
+  document.getElementById("hecho").innerHTML = hecho;
+}
+const fondo = (
+  <img 
+    className='fondo'
+    alt='fondoOceano'
+    src='/imagenes/fondoOceano.png'
+  />
+);
+
+
+
+const hechosSobreAnimales = (
+  <div>
+    <h1>{titulo === "" ? "Click Sobre un Animal" : titulo}</h1>
+    {mostrarFondo && fondo}
+    <p id='hecho'></p>
+    <div className="animales">
+      {imagenes}
+    </div>
+  </div>
+);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {hechosSobreAnimales}
   </React.StrictMode>
 );
 
